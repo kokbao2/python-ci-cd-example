96b08068952ca5325bd7f3ed8b87e7ec567435d2
kokbao2 <kokbao2@gmail.com>
2025-04-06 03:28:12 +0800

    Initial Python CI/CD setup

--- test_main.py
+++ test_main.py
@@ -5,6 +5,7 @@ class TestAddFunction(unittest.TestCase):
     def test_add(self):
         self.assertEqual(add(2, 3), 5)
         self.assertEqual(add(-1, 1), 0)
+        self.assertEqual(add(3, 3), 7)
 
 if __name__ == '__main__':
     unittest.main()
